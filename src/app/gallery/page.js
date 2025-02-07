e1813ea9a723be5a47836b0ee7460ff34e42dfaa
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-02-07 23:55:44 +0600

    meta data and absoulate path

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -1,13 +1,18 @@
 import Image from "next/image";
+import nextImg from '@/assets/2061ae24cb66e096cc069a26f0c1822a_800x.webp'
+import Navbar from "@/components/Navbar";
 
 
 const GalleryPage = () => {
     return (
         <div className="text-center">
+            <Navbar />
             <h1>Regular image tag</h1>
             <img width={500} height={500} className="mx-auto" src="https://consultantsinabox.com/cdn/shop/files/2061ae24cb66e096cc069a26f0c1822a_800x.png?v=1715463267" alt="" />
             <h1>Nextjs image Component</h1>
             <Image width={500} className="mx-auto" height={500} src='https://consultantsinabox.com/cdn/shop/files/2061ae24cb66e096cc069a26f0c1822a_800x.png?v=1715463267' alt="next image" />
+            <h1>Local image Component</h1>
+            <Image width={500} className="mx-auto" height={500} src={nextImg} alt="next image" />
         </div>
     );
 };
